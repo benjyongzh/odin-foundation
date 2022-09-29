7a10ef3a53d582c2b15f1c4d2096bf0117f9e11f
Benjamin Yong <benj_yongzh@hotmail.com>
2022-09-29 22:14:20 +0800

    Complete exercises 05, 06, and 07

--- javascript/javascript-exercises/05_sumAll/sumAll.js
+++ javascript/javascript-exercises/05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) return "ERROR";
+
+    if (isNaN(a) || isNaN(b)) return "ERROR";
+
+    if (typeof(a) === "string" || typeof(b) === "string") return "ERROR";
+
+    //ending condition
+    if (a === b){
+        return a;
+    }
+
+    //rearranging big and small number
+    let big;
+    let small;
+    if (a > b){
+        [big, small] = [a,b];
+    } else {
+        [big, small] = [b,a];
+    }
+
+
+    //recursion
+    return (big + sumAll(small, big-1));
 
 };
+
+//console.log(sumAll(4,1));
 
 // Do not edit below this line
 module.exports = sumAll;
--- javascript/javascript-exercises/06_leapYears/leapYears.js
+++ javascript/javascript-exercises/06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    return year %4 !== 0 ? false
+    : year %400 === 0 ? true
+    : year %100 !== 0 ? true
+    : false;
+
+    /* if (year %4 === 0){
+        if (year %400 ===0){
+            return true;
+        } else if (year %100===0){
+            return false;
+        } else return true;
+    } else {
+        return false;
+    } */
 
 };
+
+//console.log(leapYears(1997))
 
 // Do not edit below this line
 module.exports = leapYears;
