357362701d82fd40ffeac59f3b55ae0cc9ff94eb
Benjamin Yong <benj_yongzh@hotmail.com>
2022-10-02 17:46:34 +0800

    Complete exercise 08.

--- javascript/javascript-exercises/08_calculator/calculator.js
+++ javascript/javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a ,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, number) => total + number, 0);
 };
 
-const multiply = function() {
+const multiply = function(array) {
+	return array.reduce((total, number) => total * number, 1);
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	//recursion
+  if (number <= 1){
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  };
 };
 
 // Do not edit below this line
