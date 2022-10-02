5972bffa9af705a82155c1e57e443ec580d0ddbb
Benjamin Yong <benj_yongzh@hotmail.com>
2022-10-02 15:14:30 +0800

    Add footer div with stats. Improve highlighting style.

--- javascript/projects/etch-a-sketch/etch.js
+++ javascript/projects/etch-a-sketch/etch.js
@@ -1,11 +1,16 @@
 const container = document.querySelector("#grid-container");
 const refreshButton = document.querySelector(".refresh-button");
+const widthStatbox = document.querySelector("#width-value");
+const heightStatbox = document.querySelector("#height-value");
+const countStatbox = document.querySelector("#count-value");
+const paintedStatbox = document.querySelector("#painted-value");
 
 let width = 16;
 let height = 16;
 const widthLimit = 100;
 const heightLimit = 100;
 let isMouseDown = false;
+let paintCount = 0;
 
 function refresh(){
     specifyWidth();
@@ -36,27 +41,37 @@ function initialize(width, height){
     const boxes = container.querySelectorAll(".box");
     boxes.forEach(box => box.remove());
 
+    //create new grid
     container.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${height}, 1fr)`;
 
     const count = width * height;
 
+    //create new boxes
     for (let i = 0; i < count; i++){
         const box = document.createElement("div");
         box.setAttribute('id', `box-${i}`);
         // box.setAttribute('draggable', false);
         // console.log(box.getAttribute('draggable'))
         box.classList.add("box");
-        box.addEventListener("mouseover", boxActivate);
+        box.addEventListener("mouseover", paintBox);
         container.appendChild(box);
     };
 
+    //initialize stats
+    initializeStats(count);
 }
 
-function boxActivate(event){
+function paintBox(event){
     if (!isMouseDown) return;
     const box = event.currentTarget;
+    boxActivate(box);
+}
+
+function boxActivate(box){
     box.classList.add("activated");
+    paintCount +=1;
+    paintedStatbox.textContent = `Boxes painted: ${paintCount}`;
 }
 
 function mouseActivate(event){
@@ -64,7 +79,7 @@ function mouseActivate(event){
     isMouseDown = true;
     console.log(`mousedown is ${isMouseDown}`)
     if (event.currentTarget != event.target){
-        event.target.classList.add("activated");
+        boxActivate(event.target)
     };
 };
 
@@ -72,6 +87,14 @@ function mouseDeactivate(event){
     isMouseDown = false;
     console.log(`mousedown is ${isMouseDown}`)
 };
+
+function initializeStats(boxCount){
+    paintCount = 0;
+    widthStatbox.textContent = `Width: ${width}`;
+    heightStatbox.textContent = `Height: ${height}`;
+    countStatbox.textContent = `Box count: ${boxCount}`;
+    paintedStatbox.textContent = `Boxes painted: ${paintCount}`;
+}
 
 refreshButton.addEventListener('click', refresh);
 container.addEventListener("mousedown", mouseActivate);
