271b3c8654ab475845d243d54a61a391dd4417f6
Benjamin Yong <benj_yongzh@hotmail.com>
2022-10-02 18:09:47 +0800

    Complete exercise 09.

--- javascript/javascript-exercises/09_palindromes/palindromes.js
+++ javascript/javascript-exercises/09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const alphabets = [...string].filter(char => {
+        return char.toUpperCase() != char.toLowerCase();
+    })
+    .map(char => char.toLowerCase());
+    const reverseAlphabets = [...alphabets].reverse();
+    // console.log(alphabets);
+    // console.log(reverseAlphabets);
+    return alphabets.toString() === reverseAlphabets.toString();
 };
+
+//console.log(palindromes('ZZZZ car, a man, a maracaz.'));
+// console.log(palindromes('Animal loots foliated detail of stool lamina.'));
 
 // Do not edit below this line
 module.exports = palindromes;
