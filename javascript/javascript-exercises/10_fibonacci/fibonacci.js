a810075ec44a483ec947a222db5b726fe185dd06
Benjamin Yong <benj_yongzh@hotmail.com>
2022-10-02 20:11:50 +0800

    Complete exercise 10.

--- javascript/javascript-exercises/10_fibonacci/fibonacci.js
+++ javascript/javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,4 +1,9 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    //recursion
+    //fibonacci(n) = fibonacci(n-1) + fibonacci(n-2)
+    if(index <= 0) return "OOPS";
+    if(index == 1 || index == 2) return 1;
+    return fibonacci(index-1) + fibonacci(index-2);
 
 };
 
