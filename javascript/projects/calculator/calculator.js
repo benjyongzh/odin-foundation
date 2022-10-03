2eade7f5ee905f43bb652f178c15b8092cb12cf5
Benjamin Yong <benj_yongzh@hotmail.com>
2022-10-03 21:33:23 +0800

    Add basic styling. Empty button containers.

---
+++ javascript/projects/calculator/calculator.js
@@ -0,0 +1,10 @@
+const buttonContainer = document.querySelector(".button-container");
+const numberButtonContainer = document.querySelector(".number-button-container");
+const operatorButtonContainer = document.querySelector(".operator-button-container");
+const executionButtonContainer = document.querySelector(".execution-button-container");
+
+initialize(){
+    
+}
+
+initialize();
